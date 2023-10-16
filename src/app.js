f5c5ef14a3876fd7be3f10a9d9dfd15e18d733d1
Pau Fargas <142971403+PauFargas@users.noreply.github.com>
2023-10-16 19:01:27 +0000

    New Intent with Index

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function domainGenerator() {
+  let pronoun = ["the", "our", "his"];
+  let adj = ["great", "big", "last"];
+  let noun = ["jogger", "racoon", "samurai"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        let domain = pronoun[i] + adj[j] + noun[k] + ".com";
+        console.log(domain);
+      }
+    }
+  }
+}
