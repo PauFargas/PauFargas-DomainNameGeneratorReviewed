b9039d8c4f8433c6ab1f423b3027b8fbdca7de3d
Pau Fargas <142971403+PauFargas@users.noreply.github.com>
2024-02-25 18:26:23 +0000

    Código explicado y bonico

--- src/app.js
+++ src/app.js
@@ -1,31 +1,38 @@
-/* eslint-disable */
+// Importaciones de bootstrap y estilos
 import "bootstrap";
 import "./style.css";
 
+//Importaciones de elementos
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+//Combinaciones
 const pronouns = ["the", "our"];
 const adjectives = ["great", "big"];
 const nouns = ["jogger", "racoon"];
 
+//Función para generar los nombres de dominios
 function generateDomainNames() {
-  const domainNames = [];
+  const generatedDomainNames = [];
 
   for (let i = 0; i < pronouns.length; i++) {
+    //Agarra el primer pronombre
     for (let j = 0; j < adjectives.length; j++) {
+      //Agarra el primer adjetivo
       for (let k = 0; k < nouns.length; k++) {
+        //Agarra el primer nombre
         const domainName = `${pronouns[i]}${adjectives[j]}${nouns[k]}.com`;
-        domainNames.push(domainName);
+        generatedDomainNames.push(domainName); //Los junta y los añade
       }
     }
   }
-  document.getElementById("domainNamesList").innerHTML = domainNames.join(
-    "<br>"
-  );
-  return domainNames;
+  document.getElementById(
+    "domainNamesList"
+  ).innerHTML = generatedDomainNames.join("<br>"); //Se muestran aquí, el <br> es para saltar línea. Repite el proceso hasta hacer toda la combinación posible.
+  return generatedDomainNames;
 }
 
 window.onload = function() {
+  //WindowOnload para que lo haga cada vez que se recarga la página.
   generateDomainNames();
 };
